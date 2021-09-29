16b092ccf6c3e7355b99c127cc7d045f47ec01df
SamSepiool <simone.severini@outlook.it>
2021-09-29 20:53:58 +0200

    add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,127 @@
+<template>
+
+    <header>
+        <div class="container">
+
+            <div class="logo">
+                <a href="#"><img src="../assets/img/dc-logo.png" alt=""></a>
+            </div>
+            <nav>
+                <ul>
+                    <li v-for="(element, index) in menu" :key="index"><a :href="element.url">{{element.item}}</a></li>
+                </ul>
+            </nav>
+
+        </div>
+
+    </header>
+    
+</template>
+
+<script>
+export default {
+    name: 'Header',
+
+    data(){
+
+        return {
+            menu:[
+                {
+                    item: 'CHARACTERS',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    item: 'COMICS',
+                    url: '#',
+                    current: true
+                },
+                 {
+                    item: 'MOVIES',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    item: 'TV',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    item: 'GAMES',
+                    url: '#',
+                    current: false
+                },
+                   {
+                    item: 'COLLECTIBLES',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    item: 'VIDEOS',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    item: 'FANS',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    item: 'NEWS',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    item: 'SHOP',
+                    url: '#',
+                    current: false
+                },                
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/variables";
+
+header {
+    background-color: $white;
+    color: black;
+    font-weight: bold;
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        ul{
+            display: flex;
+
+            li{
+                display: flex;
+                align-items: center;
+                margin: 0 .625rem;
+            }
+
+            a{
+                display: inline-block;
+                padding: 3.4375rem 0;
+
+                &:active{
+                    color: $blue;
+                    box-shadow: 0px -5px 0px inset $blue; 
+                }
+            }
+        }
+    }
+
+   
+
+}
+
+
+
+
+
+</style>
